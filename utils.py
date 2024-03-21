e9579a6371eae40f0e90c1354dd0731883c461cf
RaineLegary <rainethcubed@gmail.com>
2024-03-21 13:34:03 -0600

    proxy details updated

--- utils.py
+++ utils.py
@@ -32,6 +32,7 @@ proxy_port = 5555
 #assumes csx1.ucalgary.ca is the host
 proxy_details = {
     "csx1.uc.ucalgary.ca": 5555,
+    "csx2.uc.ucalgary.ca": 5555,
     "csx3.uc.ucalgary.ca": 5555
 }
 
