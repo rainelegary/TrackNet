21263902d040f6a86e949271d3760e8e22bb2ef0
Salim Kassouri <salimkassouri2000@gmail.com>
2024-03-21 13:17:48 -0600

    Update proxy.py
    debugging updating backup proxy with master server

--- proxy.py
+++ proxy.py
@@ -246,8 +246,7 @@ class Proxy:
                 if heartbeat.HasField("master_host"):
                     # if no master server or new master server
                     if self.master_socket is None or self.master_socket.getpeername()[0] != heartbeat.master_host:
-                        LOGGER.debug("Updating master server ...")
-						LOGGER.info("slave sockets:  ", self.slave_scokets, "items: ",self.slave_scokets.items())
+						LOGGER.debug("Updating master server ...")
                         for _, slave in self.slave_sockets.items():
                             if slave.getpeername()[0] == heartbeat.master_host:
                                 self.master_socket = slave
