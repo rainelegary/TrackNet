3be43b1966b14c2f862dac42d4337ee1d0656086
RaineLegary <rainethcubed@gmail.com>
2024-03-22 13:23:44 -0600

    proxy details

--- utils.py
+++ utils.py
@@ -31,9 +31,8 @@ proxy_port = 5555
 
 #assumes csx1.ucalgary.ca is the host
 proxy_details = {
-    "csx1.uc.ucalgary.ca": 5555,
-    "csx2.uc.ucalgary.ca": 5555,
-    "csx3.uc.ucalgary.ca": 5555
+    "csa1.ucalgary.ca": 5555,
+    "csa2.ucalgary.ca": 5555,
 }
 
 exit_flag = False
