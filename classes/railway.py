b755db28fee1bf8f60c00364991729096471dbf6
McKenzie Lefebvre <109600778+mckenzie-lefe@users.noreply.github.com>
2024-04-06 14:44:58 -0600

    Document and clean up railway

--- classes/railway.py
+++ classes/railway.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 from .junction import *
 from .route import *
 from .train import *
@@ -27,18 +26,26 @@ LOGGER = logging.getLogger(__name__)
 
 
 class Railway:
-    """Represents the entire railway map, containing junctions and tracks.
+    """Represents the entire railway system, including all junctions, tracks, and trains within the network.
 
-    Attributes:
-        junctions (dict): Stores junctions by their names.
-        tracks (dict): Stores all tracks in the railway.
-        trains (dict): Stores trains by their name
+    Attributes
+    ----------
+    map : Railmap
+        A composition of the Railmap class that stores all the junctions and tracks within the railway system. Junctions and tracks within the Railmap can be accessed as map.junctions and map.tracks, respectively.
+    trains : dict
+        Stores all trains by their names. Each train is associated with its name as the key.
+    train_counter : int
+        A counter used to assign unique names to newly created trains.
     """
 
     def __init__(self, trains=None, junctions=None, tracks=None):
-        self.map = Railmap(
-            junctions, tracks
-        )  # Composition: Railway has a Railmap; Track & Junction are now accessed as map.tracks and map.junctions
+        """Initializes the Railway system with optional trains, junctions, and tracks.
+
+        :param trains: A dictionary of trains where keys are train names and values are train lengths. Defaults to None.
+        :param junctions: A list of junction names to initialize the railway map. Defaults to None.
+        :param tracks: A list of tuples where each tuple contains information about a track (start junction, end junction, and length). Defaults to None.
+        """
+        self.map = Railmap(junctions, tracks)  # Composition: Railway has a Railmap; Track & Junction are now accessed as map.tracks and map.junctions
         self.trains = {}  # store trains by name
         self.train_counter = 0
 
@@ -47,11 +54,10 @@ class Railway:
                 self.add_train(train_name, train_length)
 
     def create_new_train(self, length: int, origin_id: str):
-        """
-        Creates a new Train object with the specified length and adds it to the list of trains.
+        """Creates and parks a new train at the specified origin junction.
 
-        :param len: The length of the new train.
-        :param origin_id: Name of the junction where the train starts
+        :param length: The length of the new train.
+        :param origin_id: The ID (name) of the junction where the train will start.
         :return: The newly created Train object.
         """
         new_name = "Train" + str(self.train_counter)
@@ -62,23 +68,28 @@ class Railway:
         self.trains[new_name] = new_train
         return new_train
 
-    def add_train(self, name, length):
-        """Adds a new train to the map."""
+    def add_train(self, name: str, length: int):
+        """Adds a new train to the railway map.
+
+        :param name: The name of the new train.
+        :param length: The length of the new train.
+        """
         if name not in self.trains:
             self.trains[name] = Train(name, length)
         else:
             raise Exception(f"Train {name} already exists.")
 
-    def update_train(self, train, state, location_msg: TrackNet_pb2.Location, route_msg: TrackNet_pb2.Route):
-        	
+    def update_train(self, train: Train, state, location_msg: TrackNet_pb2.Location, route_msg: TrackNet_pb2.Route):
+        """Updates the state and location of a specified train based on the provided protobuf messages.
+
+        :param train: The Train object to update.
+        :param state: The new state of the train.
+        :param location_msg: A protobuf message containing the new location information.
+        :param route_msg: A protobuf message containing the new route information.
+        """
         LOGGER.debug(f" train name: {train.name} \n train location={train.location} \n new location={location_msg}")
 
         # check if new track
-        # if (train.location.front_cart["track"] is None or train.location.front_cart["track"].name != location_msg.front_track_id):
-        #     if state not in [TrainState.PARKED, TrainState.PARKING]:
-        #         # add to new track
-        #         self.map.tracks[location_msg.front_track_id].add_train(train)
-        
         if (train.state in [TrainState.PARKED, TrainState.PARKING]) and (state in [TrainState.RUNNING, TrainState.UNPARKING]):
             # add to new track
             self.map.tracks[location_msg.front_track_id].add_train(train)
@@ -97,28 +108,13 @@ class Railway:
             
             # remove train from track
             try:
-                self.map.tracks[train.location.back_cart["track"].name].remove_train(
-                    train.name
-                )
+                self.map.tracks[train.location.back_cart["track"].name].remove_train(train.name)
             except:
                 pass
             # add tain to new junction
             self.map.junctions[location_msg.front_junction_id].park_train(train)
 
         LOGGER.debug(f"train_state={train.state} client_state={state}")
-
-        # check if leaving junction
-        # if train.state == TrainState.PARKED and state in [
-            # TrainState.RUNNING,
-            # TrainState.UNPARKING,
-        # ]:
-            # # remove train from junction
-            # LOGGER.debug(f"Remove {train.name} from {location_msg.back_junction_id}")
-            # try:
-                # ## remove train front cart junc?
-                # self.map.junctions[location_msg.back_junction_id].depart_train(train)
-            # except Exception as exc:
-                # LOGGER.debug("ERROR: " + str(exc))
 
         updated_location = Location()
         updated_location.front_cart = {
@@ -132,7 +128,6 @@ class Railway:
             "position": location_msg.back_position,
         }
         self.trains[train.name].location = updated_location
-
         self.trains[train.name].state = state
 
         # update route
@@ -142,6 +137,11 @@ class Railway:
         # train.route = MessageConverter.route_msg_to_obj(route_msg, self.map.junctions)
 
     def set_route_for_train(self, route: TrackNet_pb2.Route, train: Train):
+        """Sets a new route for the specified train.
+
+        :param route: A protobuf Route message detailing the new route.
+        :param train: The Train object to update.
+        """
         new_route = []
         for junc in route.junctions:
             new_route.append(self.map.junctions[junc])
@@ -149,74 +149,8 @@ class Railway:
         train.location.set_track(self.train.route.get_next_track())
         LOGGER.debug(f"init track={self.train.route.get_next_track()}")
 
-    def add_train_to_track(self, train_name, track_name):
-        """Places an existing train on a specified track by name."""
-        # *Modify to remove train from parked junction if on one*
-
-        if train_name in self.trains and track_name in self.map.tracks:
-            train = self.trains[train_name]
-            self.map.tracks[track_name].add_train(train)
-            train.place_on_track(self.map.tracks[track_name])
-            print(f"Train {train_name} added to track {track_name}.")
-        else:
-            print(f"Train {train_name} not found or track {track_name} does not exist.")
-
-    def park_train_at_junction(self, train_name, junction_name):
-        """Parks an existing train at a specified junction by name."""
-        if train_name in self.trains and junction_name in self.map.junctions:
-            train = self.trains[train_name]
-            junction = self.map.junctions[junction_name]
-
-            # Remove the train from its current track if it's on one
-            if train.current_track_front:
-                del train.current_track_front.trains[train.name]
-
-            junction.park_train(train)
-            train.park_at_junction(junction)
-            print(f"Train {train_name} parked at junction {junction_name}.")
-        else:
-            print(
-                f"Train {train_name} not found or junction {junction_name} does not exist."
-            )
-
     def print_map(self):
-        """Prints an overview of the map, including junctions, tracks, and parked or running trains."""
-        print("Map Overview:")
-        print("Junctions:")
-        for junction_name, junction in self.map.junctions.items():
-
-            trains_info = ", ".join([train for train in junction.parked_trains])
-            print(f"  Junction: {junction_name}, Parked Trains: [{trains_info}]")
-
-        print("Tracks:")
-        for (track_name,track,) in self.map.tracks.items():  # Adjusted to iterate through items()
-            if hasattr(track, "trains") and isinstance(track.trains, dict):  # Safety checks
-
-                running_trains = track.trains.keys()  # track.trains is a dictionary
-
-                print(f"{track.name}, Length: {track.length}, Running Trains: ")
-
-                for running_train in running_trains:
-                    train_position_front = track.trains[
-                        running_train
-                    ].location.front_cart["position"]
-                    train_position_back = track.trains[
-                        running_train
-                    ].location.back_cart["position"]
-                    train_speed = track.trains[running_train].current_speed
-                    track_speed = track.speed
-                    # ***prints the track speed*** - train speed is 0 because it is not being sent from client to server in the protobuf
-                    print(
-                        f"\t\t\t\t\t\t Train: {running_train} Speed: {track_speed} Front Position: {train_position_front} Back Position: {train_position_back}"
-                    )
-                #print("]")
-
-            else:
-                print(f"  Track: {track_name} has no running trains or is not properly initialized.")
-
-    def print_map_new(self):
-        """Prints an overview of the map, including junctions, tracks, and parked or running trains."""
-        print("Map Overview:")
+        """Prints an overview of the railway map, including details of junctions, tracks, and trains."""
 
         # Printing Junctions with parked trains
         if self.map.junctions:
